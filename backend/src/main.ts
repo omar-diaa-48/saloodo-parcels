73328a150813a4ca3156d63b5ffb438ad845a869
Omar <omar@kamelizer.com>
2023-02-07 10:18:57 +0200

    add swagger api documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const PORT = process.env.PORT;
+	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle('Saloodo API documentation')
+		.setDescription('Saloodo API documentation description')
+		.setVersion('1.0')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api-docs', app, document);
+
+
+	await app.listen(PORT);
 }
-bootstrap();
+
+bootstrap()
+	.then(() => {
+		const message = `App server is up and running, Enviornment: ${process.env.NODE_ENV}, Port: ${process.env.PORT}`;
+		console.log(message);
+	})
+	.catch((error) => {
+		console.log(error);
+	})
