e9b13fc478e3d6b63585fd54b92ae22e2a28e015
Omar <omar@kamelizer.com>
2023-02-07 19:09:33 +0200

    combine user in reducer slice

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import reducer from './root'
+import user from './slices/user'
 
 export const store = configureStore({
-	reducer: {},
+	reducer,
 })
 
 export type RootState = ReturnType<typeof store.getState>
