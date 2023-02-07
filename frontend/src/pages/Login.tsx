03dbd56ccfd75497ff47484b38d7330ab7059460
Omar <omar@kamelizer.com>
2023-02-07 18:50:03 +0200

    add login page implementation

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,8 +2,42 @@ import React from 'react'
 
 const Login = () => {
 	return (
-		<div>Login</div>
+		<section className="h-screen">
+			<div className="px-6 h-full text-gray-800">
+				<div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
+					<div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+						<form>
+							<div className="mb-6">
+								<input
+									type="text"
+									className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+									id="exampleFormControlInput2"
+									placeholder="Name"
+								/>
+							</div>
+
+							<div className="mb-6">
+								<input
+									type="password"
+									className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+									id="exampleFormControlInput2"
+									placeholder="Password"
+								/>
+							</div>
+							<div className="text-center lg:text-left">
+								<button
+									type="button"
+									className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+								>
+									Login
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
 	)
 }
 
-export default Login
+export default Login;
