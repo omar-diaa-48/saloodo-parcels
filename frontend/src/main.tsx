1458e47afb2da5183328cc77647101ff26ffff7c
Omar <omar@kamelizer.com>
2023-02-07 18:49:56 +0200

    add redux toolkit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { store } from './store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<Router>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</Router>,
 )
