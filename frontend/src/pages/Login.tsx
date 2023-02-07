621a80c3a721a9906682802dc9f044bccb7ae604
Omar <omar@kamelizer.com>
2023-02-07 20:33:19 +0200

    add login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,33 +1,58 @@
 import React from 'react'
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { loginSchema } from '../schemes/login';
+import { Controller, useForm } from 'react-hook-form';
+import { useAppDispatch } from '../store/hooks';
+import { useNavigate } from 'react-router-dom';
+import { signinAction } from '../store/slices/user';
+import { ISigninDto } from "../utils/dtos"
+import { TextInput, ValidatedInput } from '../components/shared/input';
 
 const Login = () => {
+	const navigate = useNavigate();
+	const dispatch = useAppDispatch();
+
+	const { control, formState, handleSubmit } = useForm<ISigninDto>({
+		mode: "onChange",
+		defaultValues: {},
+		resolver: yupResolver(loginSchema),
+	});
+
+	const { errors, isValid } = formState;
+
+	const loginCallback = (values: ISigninDto) => {
+		console.log({ values });
+		dispatch(signinAction(values))
+			.then(() => navigate('/home'))
+	};
+
 	return (
 		<section className="h-screen">
 			<div className="px-6 h-full text-gray-800">
 				<div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
 					<div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-						<form>
+						<form onSubmit={handleSubmit(loginCallback)}>
 							<div className="mb-6">
-								<input
-									type="text"
-									className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-									id="exampleFormControlInput2"
-									placeholder="Name"
-								/>
-							</div>
-
-							<div className="mb-6">
-								<input
-									type="password"
-									className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-									id="exampleFormControlInput2"
-									placeholder="Password"
+								<Controller
+									name="name"
+									control={control}
+									render={({ field }) => (
+										<ValidatedInput message={errors?.name?.message}>
+											<TextInput
+												{...field}
+												placeholder="name"
+												className="w-full mb-4"
+											/>
+										</ValidatedInput>
+									)}
 								/>
 							</div>
 							<div className="text-center lg:text-left">
 								<button
-									type="button"
-									className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+									type="submit"
+									className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 disabled:bg-blue-200 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+									disabled={!isValid}
 								>
 									Login
 								</button>
--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -10,3 +10,18 @@ export type ActionPayloadType<T> = {
 	type: string;
 	payload: T
 }
+
+export interface InputProps {
+	id?: string;
+	name?: string;
+	value?: any;
+	children?: React.ReactNode,
+	onClick?: any;
+	onChange?: any;
+	disabled?: boolean;
+	hidden?: boolean;
+	className?: string;
+	ref?: any;
+	style?: any;
+	readOnly?: boolean;
+}
